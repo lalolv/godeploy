e79176e3cbff2c6ecb6820c6c3312570c95ebca7
Lalo <code@lalo.im>
2021-09-26 09:50:23 +0800

    加入 mod 模式

--- main.go
+++ main.go
@@ -44,13 +44,13 @@ func deploy(c *gin.Context) {
 
 // 调用系统指令的方法，参数s 就是调用的shell命令
 func system(s string) {
-	cmd := exec.Command("/bin/sh", "-c", s) //调用Command函数
-	var out bytes.Buffer                    //缓冲字节
+	cmd := exec.Command("/bin/sh", "-c", s) // 调用Command函数
+	var out bytes.Buffer                    // 缓冲字节
 
-	cmd.Stdout = &out //标准输出
-	err := cmd.Run()  //运行指令 ，做判断
+	cmd.Stdout = &out // 标准输出
+	err := cmd.Run()  // 运行指令 ，做判断
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Printf("%s", out.String()) //输出执行结果
+	fmt.Printf("%s", out.String()) // 输出执行结果
 }
