411e788802c821fe1dc8b4c43d9fbab63afc2be3
Lalo <code@lalo.im>
2021-10-04 11:03:44 +0800

    Send to slack

--- main.go
+++ main.go
@@ -9,10 +9,14 @@ import (
 
 	"github.com/ccding/go-config-reader/config"
 	"github.com/gin-gonic/gin"
+	"github.com/lalolv/goapis"
 )
 
 var (
-	shPath string
+	shPath       string
+	slackUtil    goapis.SlackUtil
+	slackToken   string
+	slackChannel string
 )
 
 func main() {
@@ -25,6 +29,15 @@ func main() {
 	}
 
 	shPath = c.Get("shell", "path")
+
+	// slack
+	slackToken = c.Get("slack", "token")
+	slackChannel = c.Get("slack", "channel")
+	if slackToken != "" {
+		slackUtil = goapis.SlackUtil{Token: slackToken}
+		text := fmt.Sprintf("[%s] Deploy service is done.", "Startup")
+		sendSlack(text)
+	}
 
 	// Creates a gin router with default middlewares:
 	// logger and recovery (crash-free) middlewares
@@ -47,12 +60,34 @@ func deploy(c *gin.Context) {
 	}
 
 	// Call system command
-	system(fmt.Sprintf(`cd %s && sh %s.sh`, shPath, shell))
+	err = system(fmt.Sprintf(`cd %s && sh %s.sh`, shPath, shell))
+	if err != nil {
+		// slack
+		go sendSlack(err.Error())
+		// response
+		c.String(http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	// slack
+	go sendSlack("[Deploy] Success to deploy " + shell)
+
+	// response
 	c.String(http.StatusOK, "OK")
 }
 
+// Send to slack message
+func sendSlack(text string) {
+	if slackUtil.Token != "" && slackChannel != "" {
+		err := slackUtil.PostMessage(slackChannel, text)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+	}
+}
+
 // 调用系统指令的方法，参数s 就是调用的shell命令
-func system(s string) {
+func system(s string) error {
 	// 调用Command函数
 	cmd := exec.Command("/bin/sh", "-c", s)
 	// 缓冲字节
@@ -63,8 +98,10 @@ func system(s string) {
 	// 运行指令 ，做判断
 	err := cmd.Run()
 	if err != nil {
-		fmt.Println(err)
+		return err
 	}
 	// 输出执行结果
 	fmt.Printf("%s", out.String())
+
+	return nil
 }
